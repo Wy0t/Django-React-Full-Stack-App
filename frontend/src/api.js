10c68a72577e817efe5fcc48cba6fde8a6b38329
Wyot <Wyot0128@outlook.com>
2024-07-15 19:42:21 +0800

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants" 
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "https://0e316787-d53f-4448-b063-6a993f276814-dev.e1-us-east-azure.choreoapis.dev/djangoreacttutorial/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
